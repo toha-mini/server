3f640565275555bf84ddc6820fe282c5f0d43a8a
princeton <ssk0080@gmail.com>
2023-07-22 01:32:45 +0900

    Feat: springSecurityConfig 인증 적용
    각각의 url에 따른 인증절차 설정

--- src/main/java/com/example/todayshouse/config/WebSecurityConfig.java
+++ src/main/java/com/example/todayshouse/config/WebSecurityConfig.java
@@ -1,12 +1,39 @@
 package com.example.todayshouse.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.http.HttpMethod.*;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement(WebSecurityConfig::stateless)
+                .authorizeHttpRequests(authorizationRequest ->
+                        authorizationRequest
+                                .requestMatchers(POST, "/api/auth/**").permitAll()
+                                .requestMatchers(GET, "/api/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .build();
+    }
+
+    private static void stateless(SessionManagementConfigurer<HttpSecurity> httpSecuritySessionManagementConfigurer) {
+        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
 
 }
