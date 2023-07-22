ca8ee59c8d0815a12cb31398b42764d7c76a87c5
princeton <ssk0080@gmail.com>
2023-07-22 15:30:06 +0900

    Feat: jwt 활용 인증 필터 구현

--- src/main/java/com/example/todayshouse/security/Jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/todayshouse/security/Jwt/JwtAuthenticationFilter.java
@@ -1,6 +1,8 @@
 package com.example.todayshouse.security.Jwt;
 
 import com.example.todayshouse.domain.dto.request.LoginRequestDto;
+import com.example.todayshouse.domain.dto.response.StatusResponseDto;
+import com.example.todayshouse.security.userdetails.UserDetailsImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -19,11 +21,19 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
+    private final String SUCCESS_MESSAGE = "로그인 완료.";
+    private final Integer SUCCESS_CODE = 200;
+    private final String SUCCESS_STATE_MESSAGE = "OK";
+    private final String FAIL_MESSAGE = "로그인 정보가 일치하지 않습니다.";
+    private final Integer FAIL_CODE = 401;
+    private final String FAIL_STATE_MESSAGE = "Unauthorized";
+
     private final ObjectMapper objectMapper;
+    private final JwtUtil jwtUtil;
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        log.info("tryLogin");
+        log.info("try login");
 
         try {
             LoginRequestDto loginRequestDto = objectMapper.readValue(request.getInputStream(), LoginRequestDto.class);
@@ -38,10 +48,26 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+        log.info("success login");
+        UserDetailsImpl principal = (UserDetailsImpl) authentication.getPrincipal();
+        String email = principal.getUsername();
+        StatusResponseDto responseDto = new StatusResponseDto(SUCCESS_MESSAGE, SUCCESS_CODE, SUCCESS_STATE_MESSAGE);
+
+        String token = jwtUtil.createToken(email);
+        jwtUtil.writeResponseDtoToResponseBody(response, responseDto);
     }
 
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        log.info("failed login");
+
+        response.setStatus(401);
+        StatusResponseDto responseDto = new StatusResponseDto(FAIL_MESSAGE, FAIL_CODE, FAIL_STATE_MESSAGE);
+        writeResponseDtoToResponseBody(response, responseDto);
+    }
+
+    private void writeResponseDtoToResponseBody(HttpServletResponse response, StatusResponseDto responseDto) throws IOException {
+        response.getWriter().write(objectMapper.writeValueAsString(responseDto));
     }
 
 }
