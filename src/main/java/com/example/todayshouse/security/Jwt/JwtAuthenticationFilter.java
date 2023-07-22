4ba9a9ca6c36dd561650940f71f4a2025c3a4062
princeton <ssk0080@gmail.com>
2023-07-22 10:42:44 +0900

    Feat: 로그인 시도 메서드
    attemptAuthentication 메서드 오버라이드 통해 로그인 시도 메서드 구현

--- src/main/java/com/example/todayshouse/security/Jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/todayshouse/security/Jwt/JwtAuthenticationFilter.java
@@ -1,4 +1,47 @@
 package com.example.todayshouse.security.Jwt;
 
-public class JwtAuthenticationFilter {
+import com.example.todayshouse.domain.dto.request.LoginRequestDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        log.info("tryLogin");
+
+        try {
+            LoginRequestDto loginRequestDto = objectMapper.readValue(request.getInputStream(), LoginRequestDto.class);
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginRequestDto.getEmail(), loginRequestDto.getPassword());
+            return getAuthenticationManager().authenticate(authenticationToken);
+        } catch (IOException e) {
+            log.info("error={}", e.getMessage());
+            throw new RuntimeException("Authentication failed: " + e.getMessage(), e);
+        }
+
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+    }
+
 }
