e5d62dcfe429c4fca9eabca9945df61d65e42c25
princeton <ssk0080@gmail.com>
2023-07-22 15:29:00 +0900

    Feat: UserDetails, UserDetailsService 개발

--- src/main/java/com/example/todayshouse/security/userdetails/ServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package com.example.todayshouse.security.userdetails;
-
-public class ServiceImpl {
-}
---
+++ src/main/java/com/example/todayshouse/security/userdetails/UserDetailsImpl.java
@@ -0,0 +1,57 @@
+package com.example.todayshouse.security.userdetails;
+
+import com.example.todayshouse.domain.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Getter
+@AllArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private final Member member;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+//        String role = member.getRole().getValue();
+//        authorities.add(new SimpleGrantedAuthority(role));
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
