baa54e6645c6aeb591d50876f04ddd6f1a98a4d7
YiHwanRyu <yihwandev@gmail.com>
2023-07-24 11:40:08 +0900

    Update: 게시물 작성 구현

--- src/main/java/com/example/todayshouse/controller/PostController.java
+++ src/main/java/com/example/todayshouse/controller/PostController.java
@@ -1,4 +1,35 @@
 package com.example.todayshouse.controller;
 
+import com.example.todayshouse.domain.dto.request.PostRequestDto;
+import com.example.todayshouse.domain.dto.response.MessageResponseDto;
+import com.example.todayshouse.security.userdetails.UserDetailsImpl;
+import com.example.todayshouse.service.PostService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j(topic = "PostController")
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping("/posts")
+    public ResponseEntity<MessageResponseDto> createPost(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                                        @RequestPart("content") @Valid PostRequestDto requestDto,
+                                                        @RequestPart(value = "titleImage") MultipartFile titleImgMultiPartFile,
+                                                        @RequestPart(value = "subImage1", required = false) MultipartFile subImg1MultiPartFile,
+                                                        @RequestPart(value = "subImage2", required = false) MultipartFile subImg2MultiPartFile
+    ) {
+        return postService.createPost(userDetails.getMember(), requestDto, titleImgMultiPartFile, subImg1MultiPartFile, subImg2MultiPartFile);
+    }
 }
--- src/main/java/com/example/todayshouse/domain/dto/request/PostRequestDto.java
+++ src/main/java/com/example/todayshouse/domain/dto/request/PostRequestDto.java
@@ -1,19 +1,10 @@
 package com.example.todayshouse.domain.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class PostRequestDto {
-
     private String content;
-
-    @NotBlank
-    private String titleImage;
-
-    private String subImage1;
-
-    private String subImage2;
 }
